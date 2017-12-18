87b54148e9eba4b0b459860d1e05419666d82df2
Dan Bjorge <dan@dbjorge.net>
2017-12-17 23:32:59 -0800

    Set up a basic 404 page and enable pushstate

---
+++ src/components/NotFoundPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="not-found-container">
+    <h1>Page not found!</h1>
+    <router-link to="/">Take me home!</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NotFoundPage'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
