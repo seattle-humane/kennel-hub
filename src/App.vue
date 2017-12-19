19f2009603e69905529707ddce969e98b115503a
Dan Bjorge <dan@dbjorge.net>
2017-12-18 23:50:12 -0800

    Add a "Kennel Hub" header

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
 <template>
   <div id="app">
-    <img src="./assets/seattle-humane-logo-color-249x110.jpg">
+    <div id="header">
+      <img id="header-logo" alt="Seattle Humane Logo" src="./assets/seattle-humane-logo-color-249x110.jpg">
+      <div id="header-site-title">
+        <h1>Kennel Hub</h1>
+      </div>
+    </div>
     <router-view/>
   </div>
 </template>
@@ -19,5 +24,17 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+#header {
+  display: inline-flex;
+  flex-flow: row wrap;
+  align-items: center;
+}
+#header-logo {
+  padding-right: 30px;
+}
+#header-site-title {
+  display: inline-flex;
+  flex-direction: column;
 }
 </style>
